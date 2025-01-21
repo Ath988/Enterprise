f868876758720635df4d946f6bd3a3395b1a9cee
eminarikan <muhammeteminarikan@gmail.com>
2025-01-21 12:03:59 +0300

    initial

---
+++ src/test/java/com/bilgeadam/enterprise/EnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilgeadam.enterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

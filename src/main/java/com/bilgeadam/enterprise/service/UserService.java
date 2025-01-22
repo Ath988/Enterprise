0e28f3e27b166c8de53cccdcdff0096effd6d70c
Alexander Joseph Walker <a.j.walker99@outlook.com>
2025-01-22 12:20:40 +0000

    GlobalExceptionHandler eklemesi yapildi! Kullanici kayit/Giris islemleri hatalarin yonetilmesi saglandi

--- src/main/java/com/bilgeadam/enterprise/service/UserService.java
+++ src/main/java/com/bilgeadam/enterprise/service/UserService.java
@@ -3,6 +3,8 @@ package com.bilgeadam.enterprise.service;
 import com.bilgeadam.enterprise.dto.request.LoginRequestDto;
 import com.bilgeadam.enterprise.dto.request.RegisterRequestDto;
 import com.bilgeadam.enterprise.entity.User;
+import com.bilgeadam.enterprise.exception.EnterpriseException;
+import com.bilgeadam.enterprise.exception.ErrorType;
 import com.bilgeadam.enterprise.repository.UserRepository;
 import com.bilgeadam.enterprise.util.enums.EUserState;
 import lombok.RequiredArgsConstructor;
@@ -20,8 +22,7 @@ public class UserService {
     public String doLogin(LoginRequestDto dto) {
         Optional<User> userOptional = userRepository.findByEmail(dto.email());
         if (userOptional.isEmpty() || !passwordEncoder.matches(dto.password(), userOptional.get().getPassword())) {
-            //TODO: error exception handling eklenmeli!
-            return "Invalid email or password";
+            throw new EnterpriseException(ErrorType.LOGIN_ERROR);
         }
         User user = userOptional.get();
         return "dummy_token"; //TODO: user Token donmeli!
@@ -30,14 +31,10 @@ public class UserService {
     public Boolean doRegister(RegisterRequestDto dto) {
         Optional<User> userOptional = userRepository.findByEmail(dto.email());
         if (userOptional.isPresent()) {
-            //!Kullanici zaten sisteme kayitli
-            //TODO: error exception handling eklenmeli!
-            return false;
+            throw new EnterpriseException(ErrorType.REGISTER_ERROR);
         }
         if (!dto.password().equals(dto.rePassword())) {
-            //!Sifreler uyusmuyor
-            //TODO: error exception handling eklenmeli!
-            return false;
+            throw new EnterpriseException(ErrorType.INVALID_PASSWORD);
         }
         User user = User.builder()
                 .email(dto.email())

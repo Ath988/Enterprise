644d6dc54dc4cfa0001dd3fde3b64a59b71d534d
Can <ismailcan.aydin@bilgeadamboost.com>
2025-01-24 17:30:11 +0300

    Proje için temel CRUD işlemleri tamamlandı.

--- ProjectManagementService/src/main/java/com/projectmanagementservice/controllers/ProjectController.java
+++ ProjectManagementService/src/main/java/com/projectmanagementservice/controllers/ProjectController.java
@@ -1,11 +1,16 @@
 package com.projectmanagementservice.controllers;
 
+import com.projectmanagementservice.dto.request.PageRequestDTO;
 import com.projectmanagementservice.dto.request.ProjectSaveRequestDTO;
+import com.projectmanagementservice.dto.request.ProjectUpdateRequestDTO;
+import com.projectmanagementservice.entities.Project;
 import com.projectmanagementservice.services.ProjectService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.projectmanagementservice.constants.Endpoints.*;
 
@@ -22,4 +27,29 @@ public class ProjectController
 
         return ResponseEntity.ok(projectService.save(dto));
     }
+
+    @DeleteMapping(DELETE)
+    public ResponseEntity<Boolean> delete(Long id){
+
+        return ResponseEntity.ok(projectService.delete(id));
+    }
+
+    @PutMapping(UPDATE)
+    public ResponseEntity<Boolean> update(@RequestBody ProjectUpdateRequestDTO dto){
+
+        return ResponseEntity.ok(projectService.update(dto));
+    }
+
+    @PostMapping(FIND_ALL)
+    public ResponseEntity<List<Project>> findAll(@RequestBody PageRequestDTO dto){
+
+        return ResponseEntity.ok(projectService.findAllByNameContainingIgnoreCaseAndStatusIsNotAndAuthIdOrderByNameAsc(dto));
+    }
+
+    @PostMapping(FIND_BY_ID)
+    public ResponseEntity<Project> findByIdAndAuthId(Long id){
+
+        return ResponseEntity.ok(projectService.findByIdAndAuthId(id));
+    }
+
 }

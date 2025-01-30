addbc02fa2fb9aec056c7448c224c18e06e70bf0
Taynis910 <mehmet_tufan910@hotmail.com>
2025-01-30 13:20:39 +0300

    Log filter revize edildi. Elasticseach , Kibana , Logstash entegre edildi.

--- LogService/src/main/java/com/bilgeadam/service/TraceIdGenerator.java
+++ LogService/src/main/java/com/bilgeadam/service/TraceIdGenerator.java
@@ -1,17 +1,18 @@
 package com.bilgeadam.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+import static com.bilgeadam.constants.LoggingConstants.X_TRACE_ID;
+
 @Service
 public class TraceIdGenerator {
-	public String generateRadomTraceId() {
+	public void generateRandomTraceId(HttpServletRequest request) {
 		String traceId = UUID.randomUUID().toString();
-		
-		MDC.put("X_TRACE_ID", traceId);
-		return traceId;
+		MDC.put(X_TRACE_ID, traceId);
 	}
 	
 }

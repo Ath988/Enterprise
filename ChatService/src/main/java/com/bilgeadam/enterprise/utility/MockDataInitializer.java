6f13cf45ab97360dcac1624af3f0414374e04e9c
alperenbcv <alperenbcv@gmail.com>
2025-01-30 08:34:22 +0300

    ChatService ms added

--- ChatService/src/main/java/com/bilgeadam/enterprise/utility/MockDataInitializer.java
+++ ChatService/src/main/java/com/bilgeadam/enterprise/utility/MockDataInitializer.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.*;
 
-@Component
+//@Component
 @RequiredArgsConstructor
 public class MockDataInitializer implements ApplicationRunner {
 	

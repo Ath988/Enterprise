4256687fe989b82eb7824c08d8903b0b43ffebb1
Tuğba Gül Altun <154000029+tugbagulaltun@users.noreply.github.com>
2025-02-07 16:11:53 +0300

    Varlıkların Repositoryleri oluşturuldu. Service katmanlarına başlandı. JWT sınıfları eklendi(hatalara bakılacak)

--- FinanceService/src/main/java/com/bilgeadam/config/security/JwtTokenFilter.java
+++ FinanceService/src/main/java/com/bilgeadam/config/security/JwtTokenFilter.java
@@ -53,7 +53,6 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
             SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         }
-
         filterChain.doFilter(request, response);
     }
 }
--- FinanceService/src/main/java/com/bilgeadam/config/security/SecurityConfig.java
+++ FinanceService/src/main/java/com/bilgeadam/config/security/SecurityConfig.java
@@ -3,6 +3,10 @@ package com.bilgeadam.config.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration

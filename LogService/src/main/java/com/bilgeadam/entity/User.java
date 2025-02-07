365373082b04c1c5db260024b26fb557a8198eaa
Taynis910 <mehmet_tufan910@hotmail.com>
2025-02-07 09:35:47 +0300

    Log controller oluşturuldu

---
+++ LogService/src/main/java/com/bilgeadam/entity/User.java
@@ -0,0 +1,12 @@
+package com.bilgeadam.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class User {
+	private String userId;
+	private String name;
+	private String surname;
+}
